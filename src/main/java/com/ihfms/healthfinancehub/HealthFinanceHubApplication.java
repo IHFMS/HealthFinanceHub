66566410c6614b81e4a70181ada5f4a150a8cb9b
John Trevor <softwarerecess09@gmail.com>
2024-03-21 18:21:30 +0300

    Initial commit

---
+++ src/main/java/com/ihfms/healthfinancehub/HealthFinanceHubApplication.java
@@ -0,0 +1,13 @@
+package com.ihfms.healthfinancehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthFinanceHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthFinanceHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ihfms/healthfinancehub/HealthFinanceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihfms.healthfinancehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthFinanceHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
