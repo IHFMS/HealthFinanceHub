fdbea80a624885f78c582baa076b5b74d800590a
John Trevor <softwarerecess09@gmail.com>
2024-04-08 18:27:20 +0300

    Added MessageConfig.java and updated messaging module

--- src/main/java/com/ihfms/healthfinancehub/messagingmodule/messagecontroller/ChatController.java
+++ src/main/java/com/ihfms/healthfinancehub/messagingmodule/messagecontroller/ChatController.java
@@ -1,11 +1,14 @@
 package com.ihfms.healthfinancehub.messagingmodule.messagecontroller;
 
+import com.ihfms.healthfinancehub.messagingmodule.messagemodel.ChatMessage;
 import com.ihfms.healthfinancehub.messagingmodule.messagerepo.MessageRepo;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 // models a chat room that users can go to, to receive messages
 @RestController
@@ -18,12 +21,17 @@ public class ChatController {
         this.messageRepo = messageRepo;
     }
 
-    @GetMapping("/receive-message")
+    @GetMapping("/receive-messages")
     public ModelAndView receiveMessage(
             ModelMap map
     ) {
         map.addAttribute("messages", messageRepo.getMessages());
         return new ModelAndView("chat", map);
+    }
+
+    @GetMapping("/receive-message")
+    public List<ChatMessage> receiveMessage() {
+        return messageRepo.getMessages();
     }
 
 }
