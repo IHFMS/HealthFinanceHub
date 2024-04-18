f9cfcd72b52e8daf4e771a8eb49aed4b75fc8266
1999esther <99013648+1999esther@users.noreply.github.com>
2024-04-18 10:32:10 +0300

    Updates TransactionService

--- src/main/java/com/ihfms/healthfinancehub/HealthFinanceHubApplication.java
+++ src/main/java/com/ihfms/healthfinancehub/HealthFinanceHubApplication.java
@@ -1,13 +1,30 @@
-package com.ihfms.healthfinancehub;
+//package com.ihfms.healthfinancehub;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@SpringbootApplication
+//public class HealthFinanceHubApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(HealthFinanceHubApplication.class, args);
+//    }
+//
+//
+//}
 
+package com.ihfms.healthfinancehub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
 
 @SpringBootApplication
 public class HealthFinanceHubApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(HealthFinanceHubApplication.class, args);
+
     }
 
 
