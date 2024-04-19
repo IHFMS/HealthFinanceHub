771cf63dda52c171dfe0afd30bc4083842513688
MulungiSteven <mulungisteven777@gmail.com>
2024-04-19 15:15:25 +0300

    newpatient

--- src/main/java/com/ihfms/healthfinancehub/healthmodule/services/PatientService.java
+++ src/main/java/com/ihfms/healthfinancehub/healthmodule/services/PatientService.java
@@ -1,32 +1,29 @@
 package com.ihfms.healthfinancehub.healthmodule.services;
 
 import com.ihfms.healthfinancehub.healthmodule.models.Patient;
-import com.ihfms.healthfinancehub.healthmodule.repos.PatientRepository;
+import com.ihfms.healthfinancehub.healthmodule.repos.PatientRepositoryInterface;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class PatientService{
+public class PatientService {
 
-    private final PatientRepository patientRepository;
+    private final PatientRepositoryInterface patientRepository;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepositoryInterface patientRepository) {
         this.patientRepository = patientRepository;
     }
 
-    public void addPatient(Patient patient){
+    public void addPatient(Patient patient) {
         patientRepository.registerPatient(patient);
     }
 
-    public List<Patient> getPatientList(){
+    public List<Patient> getPatientList() {
         return patientRepository.getPatientDetails();
     }
 
-    public void addPatientRecord(Long patientId, String diagnosis, List<String> prescriptions){
+    public void addPatientRecord(Long patientId, String diagnosis, List<String> prescriptions) {
         patientRepository.addRecord(patientId, diagnosis, prescriptions);
     }
-
-
 }
